3b8b2905a6b7a26788fb4b347fe138dc7aca9bd1
Hannah Lehman <hjlehman@gmail.com>
2019-07-21 22:48:33 -0400

    Onto the second part. It compiles but is still a wip

--- specialMath/specialMath.py
+++ specialMath/specialMath.py
@@ -5,6 +5,6 @@ def specialMath(n):
 		return 0
 	elif(n==1):
 		return 1
-	return n + specialMath(n-1)
+	return n + specialMath(n-1)	+ specialMath(n-2)
 
 print specialMath(int(sys.argv[1]))
--- specialMath/yay_recursion/src/main/scala/Main.scala
+++ specialMath/yay_recursion/src/main/scala/Main.scala
@@ -3,14 +3,20 @@ import scala.annotation.tailrec
 object Main extends App {
 
   def specialMath(n: Int): Int = {
-    @tailrec
-    def iterator1(x: Int, result1: Int): Int =
-      if (x == 0) 0
-      else if (x == 1) result1
-      else iterator1(x - 1, result1 + x)
-
-    iterator1(n, 1)
+    iterator(iterator2(n, 1), 1)
   }
+
+  @tailrec
+  private def iterator(x: Int, result1: Int): Int =
+    if (x == 0) 0
+    else if (x == 1) result1
+    else iterator(x - 1, result1 + x)
+
+  @tailrec
+  private def iterator2(x: Int, result2: Int): Int =
+    if (x == 0) 0
+    else if (x == 1) result2
+    else iterator2(x - 2, result2 + x)
 
   println(specialMath(7))
 }
